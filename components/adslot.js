43c34ba5cf3593253914e89f72726f84cf563071
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-08 18:38:03 +0100

    Update adslot.js

--- components/adslot.js
+++ components/adslot.js
@@ -1,1 +1,7 @@
-
+export default function AdSlot() {
+  return (
+    <div className="hidden my-6 p-4 border-2 border-dashed border-gray-200 rounded-lg text-center text-gray-400">
+      Ad Slot - Hidden until AdSense approval
+    </div>
+  )
+}
