6780363879d09143a0b52882885bf082eddd7d36
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-08 19:27:07 +0100

    Delete components directory

--- components/adslot.js
+++
@@ -1,7 +0,0 @@
-export default function AdSlot() {
-  return (
-    <div className="hidden my-6 p-4 border-2 border-dashed border-gray-200 rounded-lg text-center text-gray-400">
-      Ad Slot - Hidden until AdSense approval
-    </div>
-  )
-}
--- components/citationbar.js
+++
@@ -1,8 +0,0 @@
-export default function CitationBar({ source }) {
-  return (
-    <div className="p-3 bg-blue-50 border border-blue-200 rounded-lg text-xs mb-4">
-      <p><b>Medically Reviewed Source:</b> {source}</p>
-      <p className="text-gray-500 mt-1">This is general information. Talk to your healthcare provider.</p>
-    </div>
-  )
-    }
